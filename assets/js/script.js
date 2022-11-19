a6320160402020e7af5ca3a665c92286d69ae0cc
Sbipul <bipulcroy121@gmail.com>
2022-11-19 22:43:30 +0600

    color updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,20 +38,37 @@ if (moreButton) {
 // more section end
 
 
+let dark =  JSON.parse(window.localStorage.getItem("dark"));
 let themeButton = document.querySelector(".theme")
-let active = true;
-const themeChange =()=>{
-  active = !active
-  if (active) {
+const setTheme = (dark) =>{
+  if (!dark) {
     themeButton.innerHTML = `<i class="lar la-sun fs-4"></i>`
     document.documentElement.style.setProperty('--color-primary', 'white');
     document.documentElement.style.setProperty('--color-black', 'black');
     document.documentElement.style.setProperty('--color-secondary', 'rgb(248 248 248)');
-  } else {
+  
+} else {
     themeButton.innerHTML = `<i class="lar la-moon fs-4"></i>`
     document.documentElement.style.setProperty('--color-primary', 'black');
     document.documentElement.style.setProperty('--color-black', 'white');
     document.documentElement.style.setProperty('--color-secondary', 'rgb(45 45 45)');
-  }
-  
 }
+}
+setTheme(dark)
+
+let active = true;
+const themeChange =()=>{
+  active = !active
+  if (active) {
+    window.localStorage.setItem("dark", JSON.stringify(true));
+    setTheme(true)
+  } else {
+    window.localStorage.setItem("dark", JSON.stringify(false));
+    setTheme(false)
+  }
+
+}
+
+
+
+
