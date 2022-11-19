7f532ee6f08c89c91dc137a26377e3f08172c2fe
Sbipul <bipulcroy121@gmail.com>
2022-11-19 10:42:40 +0600

    responsive complete

--- assets/components/faq.js
+++ assets/components/faq.js
@@ -1,27 +1,27 @@
 const faq = document.querySelector(".faq")
 if (faq) {
   faq.innerHTML = `<div class="container">
-<div class="faq-container">
-  <div class="row align-items-center">
-    <div class="col-7">
-      <h2>Any Question</h2>
-      <p>
-        Answers are just an email away. Drop us a line and the right
-        person will get back to you as soon as possible
-      </p>
-      <div class="banner-buttons">
-        <button><a href="./contact.html">ask question</a></button>
+  <div class="faq-container">
+    <div class="row align-items-center">
+      <div class="col-12 col-lg-7">
+        <h2>Any Question</h2>
+        <p>
+          Answers are just an email away. Drop us a line and the right
+          person will get back to you as soon as possible
+        </p>
+        <div class="banner-buttons">
+          <button><a href="./contact.html">ask question</a></button>
+        </div>
       </div>
-    </div>
-    <div class="col-5">
-      <div class="faq-image">
-        <img
-          src="./assets/images/Capture-removebg-preview.png"
-          alt=""
-        />
+      <div class="col-12 col-lg-5">
+        <div class="faq-image">
+          <img
+            src="./assets/images/Capture-removebg-preview.png"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   </div>
-</div>
-</div>`
+  </div>`
 }
