9b53e1f1de9c6dcaaed12b5a0c9cf6e1e71239f4
SBipul <bipulcroy121@gmail.com>
2023-05-06 12:28:08 +0600

    updated

---
+++ assets/components/introduction.js
@@ -0,0 +1,28 @@
+const introduction = document.querySelector(".introduction")
+if (introduction) {
+    introduction.innerHTML = `<h3 class="introduction-title mb-2 text-secondary">
+  #about me
+</h3>
+<div class="introduction">
+  <h2>
+    my name is <span>Bipul Chandro Roy</span><br />
+    and i am a frontend developer.
+  </h2>
+  <div>
+    <p>
+      i have complete several projects with frontend
+      technologies like html,vanilla css,vanilla
+      js,typescript,react,redux and many more i mentioned in
+      home page.With 1+ year of experience,now i am very
+      confident and comfortable to work with any team where i
+      can learn more and implement ideas.
+    </p>
+    <p>
+      learning journey never end.but it was a great opportunity
+      to work several team for gaining knowledge and learning
+      technologies.with sharing logics and various algorithm and
+      fucntionalities gave me more confident.
+    </p>
+  </div>
+</div>`
+}
